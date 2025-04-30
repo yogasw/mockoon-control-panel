2da9d3e1a3ee7deef3044740344843a74c910e19
Yoga Setiawan <yogainformatika@gmail.com>
2025-04-30 07:44:53 +0700

    fix dockerfile

--- backend/src/scripts/run-generate-config.ts
+++ backend/src/scripts/run-generate-config.ts
@@ -1,5 +1,7 @@
 import { generateDynamicTraefikConfig } from '@/traefik/generate-traefik-config';
 import { EnsureRequiredFoldersAndEnv } from '@/utils/setupFolderConfig';
+import { generateStaticTraefikConfig } from '@/traefik/generateStaticTraefikConfig';
 
 EnsureRequiredFoldersAndEnv();
+generateStaticTraefikConfig()
 generateDynamicTraefikConfig(true);
