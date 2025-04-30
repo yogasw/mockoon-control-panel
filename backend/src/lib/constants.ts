afc53c0fa36deb872cecba079387466eed340e9b
Yoga Setiawan <yogainformatika@gmail.com>
2025-04-30 16:24:51 +0700

    fix typo and set name and email git

--- backend/src/lib/constants.ts
+++ backend/src/lib/constants.ts
@@ -22,8 +22,7 @@ export const SERVER_PORT = parseInt(process.env.SERVER_PORT || '3600', 10);
 export const SERVER_HOSTNAME = process.env.SERVER_HOSTNAME || '0.0.0.0';
 export const CORS_ORIGIN = process.env.CORS_ORIGIN || '*';
 export const PROXY_MODE = process.env.PROXY_MODE == 'true' || false;
-export const PROXY_BASE_URL = process.env.ROXY_BASE_URL || '';
-
+export const PROXY_BASE_URL = process.env.PROXY_BASE_URL || '';
 //Git
 export const GIT_URL = process.env.GIT_URL || '';
 export const GIT_BRANCH = process.env.BRANCH || 'main';
