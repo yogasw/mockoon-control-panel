48249032aabc562083ef1fe990f4366b28f3ae84
Yoga Setiawan <yogainformatika@gmail.com>
2025-04-30 12:04:08 +0700

    add auto migrate

--- backend/src/utils/setupFolderConfig.ts
+++ backend/src/utils/setupFolderConfig.ts
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 import { CONFIGS_DIR } from '@/lib/constants';
+import { execSync } from 'child_process';
+import * as console from 'node:console';
 
 export async function EnsureRequiredFoldersAndEnv() {
 	const foldersToCheck = [
@@ -20,7 +22,18 @@ export async function EnsureRequiredFoldersAndEnv() {
 	//check DATABASE_URL exists or not in process
 	if (!process.env.DATABASE_URL) {
 		process.env.DATABASE_URL = `file:${path.join(CONFIGS_DIR, 'db')}/db.sqlite`;
+		console.log(process.env.DATABASE_URL)
 		console.log(`DATABASE_URL not found. Using default: CONFIG_DIR/db/db.sqlite`);
+
+		//run migration
+		try {
+			console.log('Running database migration...');
+			execSync('npm run db:migrate', { stdio: 'inherit' });
+			console.log('Database migration completed.');
+		} catch (error) {
+			console.error('Error running database migration:', error);
+		}
+
 	}
 
 }
